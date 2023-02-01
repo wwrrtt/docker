4c5c8fb732e181b07f54f96f35b12f8e4492a286
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-02-01 13:52:00 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,7 @@ sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJ
 sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
 
 # 设置 nginx 伪装站
-rm -rf /usr/share/nginx/html/*
-unzip /usr/share/nginx/html/web.zip -d /usr/share/nginx/html
+rm -rf /usr/share/nginx/* && wget -c -P /usr/share/nginx "https://github.com/wwrrtt/docker/raw/main/web.zip" && unzip -o "/usr/share/nginx/web.zip" -d /usr/share/nginx/html
 
 
 # 伪装 xray 执行文件
