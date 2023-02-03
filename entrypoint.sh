af6bb8e791b12b2f88e67d75d96b5b0c57a66f32
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-02-03 17:28:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,5 +29,6 @@ unzip -o web.zip -d /usr/share/nginx/html
 # 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
 [ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
 
+cat log
 nginx
 base64 -d config > config.json; ./$xpid -config=config.json
